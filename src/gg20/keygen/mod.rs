c1b005e9918d2e5a03b843e2f03279d4bb77b345
SetarehGhorshi <ssghorsh@uwaterloo.ca>
2023-07-06 21:37:25 +0000

    cleanup

--- src/gg20/keygen/mod.rs
+++ src/gg20/keygen/mod.rs
@@ -7,8 +7,7 @@ pub mod r3;
 pub mod r4;
 mod secret_key_share;
 
-#[cfg(test)]
-pub(super) mod tests; // pub(super) so that sign module can see tests::execute_keygen
+
 
 #[cfg(feature = "malicious")]
 pub mod malicious;
