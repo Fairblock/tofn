cbad51843c1ccfaff8a395b593a662b8deee4439
SetarehGhorshi <ssghorsh@uwaterloo.ca>
2023-08-14 07:45:05 +0000

    latest

--- src/gg20/keygen/r3.rs
+++ src/gg20/keygen/r3.rs
@@ -230,7 +230,7 @@ impl Executer for R3 {
         .ek;
                     log_accuse_warn(my_keygen_id, peer_keygen_id, "invalid vss share");
                     let p = vss::Proof::generate_proof(&bls12_381::G1Projective::generator(),&key,&(self.dk*bls12_381::G1Projective::generator()),self.kij.get(peer_keygen_id.as_usize()).unwrap(),&self.dk).unwrap();
-                   let cc = serialize(&commit);
+                  // let cc = serialize(&commit);
                    //debug!("share: {:?} - commits: {:?}",share_info.share.clone(),cc );
                    vss_complaints_vec.push(ShareInfoDispute{ share: share_info.share.clone(), kij: *self.kij.get(peer_keygen_id.as_usize()).unwrap(), proof: p, commit:commit, faulter: key, accuser:my_key , accuserId:my_keygen_id.as_usize(), faulterId:peer_keygen_id.as_usize()});
                 } else {
@@ -259,8 +259,8 @@ impl Executer for R3 {
                                             self.kij.get(peer_keygen_id.as_usize()).unwrap(),
                                             &self.dk,
                                         ).unwrap();
-                                        let cc = serialize(&commit);
-                                        debug!("share: {:?} - commits: {:?}", share_info.share.clone(), cc);
+                                        // let cc = serialize(&commit);
+                                        // debug!("share: {:?} - commits: {:?}", share_info.share.clone(), cc);
                                         vss_complaints_vec.push(ShareInfoDispute {
                                             share: share_info.share.clone(),
                                             kij: *self.kij.get(peer_keygen_id.as_usize()).unwrap(),
